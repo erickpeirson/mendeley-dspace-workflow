cb9cc07515207d8d1b83f15f7a1b1a6dca3a4229
Erick <erick.peirson@asu.edu>
2013-03-14 23:14:36 -0700

    outline of project

---
+++ md_workflow.js
@@ -0,0 +1,11 @@
+//  Display all of the folders available in the group, and let the user select one
+
+//  Display all of the entries in the selected folder, including an indication of completeness
+
+//  Prompt user to complete incomplete fields, and update
+
+//  For URI fields, connect to ConceptPower API to find suggestions, and let user choose
+
+//  Display available bitstreams for each item, and allow user to upload additional bitstreams
+
+//  Check all metadata for completeness and consistency, and let user trigger export
---
+++ md_workflow.py
@@ -0,0 +1,17 @@
+#   Connect to the Mendeley API, and get all of the folders from the group
+
+#   Given a folder, create a temporary data scheme for all of the entries in the folder
+
+#   Update the temporary data, given form data
+
+#   Given an entry, download PDF from Mendeley API
+
+#   Update the temporary data with additional bitstreams, uploaded by user
+
+#   Export temporary data as a DSpace-friendly CSV + bitsreams in a zip archive
+
+class dspace_item:
+    def __init__ (self):
+        #   TODO: All of the fields...
+        return None
+
